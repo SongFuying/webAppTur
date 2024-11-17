4a805658f07ee009a11af59ac9d9cf128638cb4a
songfy <songfydesign@gmail.com>
2024-11-17 14:08:27 +0100

    11.17

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,31 @@
 import React from "react";
 
+const arrObj = [
+  { name: "jack", age: 18, job: "student" },
+  { name: "john", age: 17, job: "homeless" },
+  { name: "tom", age: 20, job: "free" },
+  { name: "tom", age: 20, job: "free" },
+  { name: "tom", age: 20, job: "free" },
+  { name: "tom", age: 20, job: "free" },
+  { name: "tom", age: 20, job: "free" },
+  { name: "tom", age: 20, job: "free" },
+  { name: "tom", age: 20, job: "free" },
+  { name: "tom", age: 20, job: "free" },
+  { name: "tom", age: 20, job: "free" },
+];
+
 export default function Home() {
-  return <div className="flex flex-col">主页:Home123</div>;
+  return (
+    <div className="grid grid-cols-5 gap-4">
+      {arrObj.map((item, index) => {
+        return (
+          <div key={index} className="p-4 border rounded">
+            <div>name: {item.name}</div>
+            <div>age: {item.age}</div>
+            <div>job: {item.job}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
